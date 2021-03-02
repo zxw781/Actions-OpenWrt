32c408cdf7db1cda301bed9abe0a15e06023e713
zxw781 <78365623+zxw781@users.noreply.github.com>
2021-03-02 16:09:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,4 @@
 #sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default 
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default 
