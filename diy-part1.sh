a4a176c39623efae82dfd1fa1e49b0ac98b545ac
zxw781 <78365623+zxw781@users.noreply.github.com>
2022-09-22 13:10:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon.git' >>feeds.conf.default
+#echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon.git' >>feeds.conf.default
